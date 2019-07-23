d55f9b2a959c1e95399ee920233cf5f19f44e01f
SAFI <amine.safi@esprit.tn>
2019-07-23 09:36:39 +0100

    missing @Repository annotation

--- src/main/java/com/delta/repositories/HoldingDeltaRepository.java
+++ src/main/java/com/delta/repositories/HoldingDeltaRepository.java
@@ -3,9 +3,11 @@ package com.delta.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.delta.models.HoldingDelta;
 
+@Repository
 public interface HoldingDeltaRepository extends MongoRepository<HoldingDelta, String> {
 
 	Page<HoldingDelta> findByReconBreak(Pageable pageable, String reconBreak);
